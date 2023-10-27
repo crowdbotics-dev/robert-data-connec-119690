cdc245e59fd0acd177aeb5fcd71290b5ff6f7f5a
Robert So <robert.so@crowdbotics.com>
2023-10-27 09:30:07 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/newglobal/api.js
+++ store/newglobal/api.js
@@ -1,8 +1,14 @@
-import axios from "axios"
-import { GLB12_USERNAME, GLB12_PASSWORD } from "react-native-dotenv"
+import axios from "axios";
+import { GLB12_USERNAME, GLB12_PASSWORD } from "react-native-dotenv";
 const newglobal = axios.create({
   baseURL: "https://bcnfjfj.com",
-  auth: { username: GLB12_USERNAME, password: GLB12_PASSWORD },
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
-export const apiService = {}
+  auth: {
+    username: GLB12_USERNAME,
+    password: GLB12_PASSWORD
+  },
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+export const apiService = {};

688d5f842cda6f460ac8c038c1f5ce6e3f68b984
Crowdbotics <engineering@crowdbotics.com>
2023-10-27 10:33:39 +0000

    Update store.

--- store/robertdataconnecAPI/api.js
+++ store/robertdataconnecAPI/api.js
@@ -1,72 +1,58 @@
-import axios from "axios";
+import axios from "axios"
 const robertdataconnecAPI = axios.create({
   baseURL: "https://robert-data-connec-119690.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
   return robertdataconnecAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+    params: { lang: payload.lang }
+  })
 }
-
 function api_v1_login_create(payload) {
-  return robertdataconnecAPI.post(`/api/v1/login/`, payload);
+  return robertdataconnecAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_signup_create(payload) {
-  return robertdataconnecAPI.post(`/api/v1/signup/`, payload);
+  return robertdataconnecAPI.post(`/api/v1/signup/`, payload)
 }
-
 function rest_auth_login_create(payload) {
-  return robertdataconnecAPI.post(`/rest-auth/login/`, payload);
+  return robertdataconnecAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_create(payload) {
-  return robertdataconnecAPI.post(`/rest-auth/logout/`);
+  return robertdataconnecAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return robertdataconnecAPI.post(`/rest-auth/password/change/`, payload);
+  return robertdataconnecAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return robertdataconnecAPI.post(`/rest-auth/password/reset/`, payload);
+  return robertdataconnecAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return robertdataconnecAPI.post(`/rest-auth/password/reset/confirm/`, payload);
+  return robertdataconnecAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
 function rest_auth_registration_create(payload) {
-  return robertdataconnecAPI.post(`/rest-auth/registration/`, payload);
+  return robertdataconnecAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_resend_email_create(payload) {
-  return robertdataconnecAPI.post(`/rest-auth/registration/resend-email/`, payload);
+  return robertdataconnecAPI.post(
+    `/rest-auth/registration/resend-email/`,
+    payload
+  )
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return robertdataconnecAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return robertdataconnecAPI.post(
+    `/rest-auth/registration/verify-email/`,
+    payload
+  )
 }
-
 function rest_auth_user_retrieve(payload) {
-  return robertdataconnecAPI.get(`/rest-auth/user/`);
+  return robertdataconnecAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return robertdataconnecAPI.put(`/rest-auth/user/`, payload);
+  return robertdataconnecAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return robertdataconnecAPI.patch(`/rest-auth/user/`, payload);
+  return robertdataconnecAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -82,4 +68,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
